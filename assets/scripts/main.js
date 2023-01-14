37401be8d21c4a105454852ef2cbb733b16026f8
elioflo <eliofloreschoque2357@gmail.com>
2023-01-14 00:10:01 -0300

    Add script

---
+++ assets/scripts/main.js
@@ -0,0 +1,10 @@
+document.getElementById('btn').addEventListener('click', (e) => {
+  console.log('CLICKED!!')
+  fetch('https://api.adviceslip.com/advice', { cache: "no-cache" })
+  .then((response) => response.json())
+  .then((json) => {
+      document.getElementById('advice').textContent = json.slip.advice
+      document.getElementById('id_advice').textContent = `Advice #${json.slip.id}`
+  })
+  .catch((err) => console.log('Solicitud fallida', err))
+})
